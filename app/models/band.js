f6db7ad71d90e72d0978bedbd4ae395c22440253
Lewis Marshall <marshy2201@yahoo.co.uk>
2018-08-27 20:48:15 +0100

    ember-cli-mirage & ember-test-selector addons added, isGreatBand property added to band model, acceptance, intergation, unit & helper tests added

--- app/models/band.js
+++ app/models/band.js
@@ -1,9 +1,15 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 
 const { attr, hasMany, Model } = DS;
 
 export default Model.extend({
   name: attr('string'),
   description: attr('string'),
-  songs: hasMany()
+  songs: hasMany(),
+
+  isGreatBand: computed('songs.[].rating', function() {
+    let goodSongs = this.get('songs').filter(song => song.get('rating') >= 4);
+    return goodSongs.length >= 2;
+  })    
 });
