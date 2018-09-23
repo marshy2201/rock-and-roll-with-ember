2bb8b10d4c8c98c194c8d377e9a970dd5ecdab7e
Lewis Marshall <marshy2201@yahoo.co.uk>
2018-09-23 18:05:24 +0100

    error template added for band route

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,12 +1,10 @@
 import Route from '@ember/routing/route';
-import wait from 'rock-and-roll-with-ember/utils/wait';
 
 export default Route.extend({
   // This is done by default
-  async model() {
-    await wait(4000);
-    return this.modelFor('bands.band');
-  },
+  // model() {
+  //   return this.modelFor('bands.band');
+  // },
 
   resetController(controller) {
     controller.setProperties({
