fc93d5940afa25bc4c9851b08a267c99614e4c6c
Lewis Marshall <marshy2201@yahoo.co.uk>
2018-10-14 21:24:38 +0100

    shorten timeout to display loading icon

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -3,7 +3,7 @@ import wait from 'rock-and-roll-with-ember/utils/wait';
 
 export default Route.extend({
   async model() {
-    await wait(3000);
+    await wait(1500);
     return this.store.findAll('band');
   },
 
